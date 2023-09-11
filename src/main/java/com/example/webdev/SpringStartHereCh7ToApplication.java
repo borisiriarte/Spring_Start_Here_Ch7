6b8b22006ee67df6c7b81c2286315a2ca24df983
Boris2514 <borismiashiro@gmail.com>
2023-09-10 20:44:46 -0400

    Updating Doc :V

---
+++ src/main/java/com/example/webdev/SpringStartHereCh7ToApplication.java
@@ -0,0 +1,13 @@
+package com.example.webdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStartHereCh7ToApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringStartHereCh7ToApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/webdev/SpringStartHereCh7ToApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webdev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStartHereCh7ToApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
